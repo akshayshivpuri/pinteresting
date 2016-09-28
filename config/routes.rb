32838be1934d7b86dc74708f4a7ba5a4aa604594
Akshay Shivpuri <shivpuri.akshay@gmail.com>
2016-09-28 15:17:34 +0530

    Added multiple pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/second-page'
-
   root 'pages#home'
 
+  get "about" => "pages#about"
+
+  get "contact" => "pages#contact"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
